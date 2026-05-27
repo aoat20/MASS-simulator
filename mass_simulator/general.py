Fixed resume cpa computation

mass_simulator/general.py
@@ -75,8 +75,8 @@ def compute_future_cpas(xy1, speed_mps1,
     # Travel time to wp
     travel_time = r/speed_mps1
     # Heading from waypoint to resumption point
-    course1_new = 90-np.atan2(goal_wp[1]-wp[1],
-                              goal_wp[0]-wp[0])
+    course1_new = 90-np.rad2deg(np.atan2(goal_wp[1]-wp[1],
+                                         goal_wp[0]-wp[0]))
     v2_xy_new = xy2 + speed_mps2*travel_time*np.array([np.sin(v2_course_rad),
                                                        np.cos(v2_course_rad)])
     cpa_m, cpa_yds, tcpa_s = compute_cpa(wp, course1_new, speed_mps1,